6672d5cd1b47752694bfa185b5f476f258bbfc0d
sumit kumar <sumit1523@gmail.com>
2019-07-10 00:19:40 +0530

    simple counter using react & redux

--- src/index.js
+++ src/index.js
@@ -4,44 +4,52 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import allReducer from './reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+    allReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 
 // STORE -> GLOBALIZED STATE
 
 // ACTION INCREMENT
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    }
-};
+// const increment = () => {
+//     return {
+//         type: 'INCREMENT'
+//     }
+// };
 
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    }
-};
-//REDUCER
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        
-    }
-};
+// const decrement = () => {
+//     return {
+//         type: 'DECREMENT'
+//     }
+// };
+// //REDUCER
+// const counter = (state = 0, action) => {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
 
-let store = createStore(counter);
+//     }
+// };
 
-// Display it in console
-store.subscribe(()=> console.log(store.getState()));
+// let store = createStore(counter);
 
-// DISPATHER
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(increment());
+// // Display it in console
+// store.subscribe(() => console.log(store.getState()));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// // DISPATHER
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(increment());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
